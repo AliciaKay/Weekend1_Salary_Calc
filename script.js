becceffaf75e0eb484bbbb29ff2c92d88a963522
Alicia Wilking <aliciakaywilking@gmail.com>
2017-09-03 18:20:18 -0500

    updates monthly totals

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var sumTotals = [];
 
 $(document).ready(onReady);
+
+var sumTotals = '';
 
 function onReady() {
     $('#submit').on('click', submitFunc);
@@ -8,15 +9,15 @@ function onReady() {
 };
 
 function submitFunc() {
-    var firstName = $("#firstName").val();
-    var lastName = $("#lastName").val();
+    var firstName = $('#firstName').val();
+    var lastName = $('#lastName').val();
     var idNumber = $('#idNumber').val();
     var jobTitle = $('#jobTitle').val();
     var annualSalary = $('#annualSalary').val();
     var monthlyCost = Math.round(annualSalary / 12);
     var markup = "<tr><td><input type='checkbox' name='record'></td><td>"+ firstName + "</td><td>" + lastName + "</td><td>" + idNumber + "</td><td>" + jobTitle + "</td><td>" + annualSalary + "</td><td class='toAdd'>" + monthlyCost + "</td></tr>";
 
-    $('table tbody').append(markup);
+    $('table tbody').append(markup);  
 
     $('#firstName').val(this.placeholder);
     $('#lastName').val(this.placeHolder);
@@ -24,12 +25,17 @@ function submitFunc() {
     $('#jobTitle').val(this.placeholder);
     $('#annualSalary').val(this.placeholder);
 
-    $('#totalCell').html('total goes here');
+    sumTotals = Number(sumTotals += monthlyCost);
+
+    $('#totalCell').html(sumTotals);
+
 };
 
 function deleteRowFunc() {
         $('table tbody').find('input[name="record"]').each(function(){
             if($(this).is(":checked")){
+                sumTotals = Number(sumTotals -= this.monthlyCost)
+                $('#totalCell').val(sumTotals);
                 $(this).parents("tr").remove();
             }
         });
