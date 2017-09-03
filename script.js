33af245f9d66b372e6f44176fca88fce50ca935f
Alicia Wilking <aliciakaywilking@gmail.com>
2017-09-03 15:02:51 -0500

    sharing repo for cohort assistance

---
+++ script.js
@@ -0,0 +1,36 @@
+var sumTotals = [];
+
+$(document).ready(onReady);
+
+function onReady() {
+    $('#submit').on('click', submitFunc);
+    $('#deleteRow').on('click', deleteRowFunc);
+};
+
+function submitFunc() {
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    var idNumber = $('#idNumber').val();
+    var jobTitle = $('#jobTitle').val();
+    var annualSalary = $('#annualSalary').val();
+    var monthlyCost = Math.round(annualSalary / 12);
+    var markup = "<tr><td><input type='checkbox' name='record'></td><td>"+ firstName + "</td><td>" + lastName + "</td><td>" + idNumber + "</td><td>" + jobTitle + "</td><td>" + annualSalary + "</td><td class='toAdd'>" + monthlyCost + "</td></tr>";
+
+    $('table tbody').append(markup);
+
+    $('#firstName').val(this.placeholder);
+    $('#lastName').val(this.placeHolder);
+    $('#idNumber').val(this.placeholder);
+    $('#jobTitle').val(this.placeholder);
+    $('#annualSalary').val(this.placeholder);
+
+    $('#totalCell').html('total goes here');
+};
+
+function deleteRowFunc() {
+        $('table tbody').find('input[name="record"]').each(function(){
+            if($(this).is(":checked")){
+                $(this).parents("tr").remove();
+            }
+        });
+    };
